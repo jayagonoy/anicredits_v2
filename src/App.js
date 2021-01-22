59dd53520660993fe18065fef1734bf508fbf349
Jay Agonoy <tcmanila@gmail.com>
2021-01-23 00:37:43 +0800

    Initial Skeletal Work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="m-auto">
+      <nav class="mx-auto">
+          <div class="w-full px-4 sm:px-6 lg:px-8">
+            <div class="">
+              <div class="flex items-center">
+                  <div class="flex items-baseline space-x-7 m-auto">
+                  <a href="http://jayagonoy.xyz" class="text-sm">jayagonoy.xyz</a>
+                    <h1 class="bg-gray-900 text-white px-3 py-2 rounded-md"><strong>anicredits<sup>v2</sup></strong></h1>
+                    <form class="flex items-center justify-between h-16">
+                      <div class="col-span-3 sm:col-span-2">
+                        <div class="mt-1 flex items-center content-center rounded-md">
+                          <div class="">
+                            <input type="text" name="search" id="search" class="border-2 w-96 px-4 py-1.5 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Search Anime..."/>
+                        </div>
+                        <span class="mt-1 ml-4">
+                          <div class="flex items-start">
+                            <div class="">
+                              <input id="comments" name="comments" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
+                            </div>
+                            <div class="ml-3 text-sm">
+                              <label for="comments" class="font-medium text-gray-700">EN Text</label>
+                            </div>
+                          </div>
+                          </span>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+              </div>
+            </div>
+          </div>
+      </nav>
+
+      <div class="container w-900 mx-auto pt-5">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-blue-800 text-white">
+                  <tr>
+                    <th scope="col" width="40%" class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                      Series Name
+                    </th>
+                    <th scope="col" width="60%" class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
+                      Attribution
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                  <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="ml-4">
+                          <div class="text-sm font-medium text-gray-900">
+                            JP Title
+                          </div>
+                          <div class="text-sm text-gray-500">
+                            EN Title
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">
+                      <input type="text" name="attribution" class="border-2 rounded border-blue-500 w-full 100vw p-5" value="JP/EN Attribution" disabled/> 
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/db2021.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+const posts = [
+    {
+      titleJP: 'JP Title 1', 
+      titleEN: 'EN Title 1',
+      contentJP: 'JP Content 1',
+      contentEN: 'EN Content 1'
+    },
+    {
+      titleJP: 'JP Title 2', 
+      titleEN: 'EN Title 2',
+      contentJP: 'JP Content 2',
+      contentEN: 'EN Content 2'
+    }
+  ];
